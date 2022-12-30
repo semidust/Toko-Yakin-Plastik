abbd44ea0f9e630f802c674f7d54bd5a55f381c1
hiskiasng <hiskiasng@gmail.com>
2022-12-30 20:01:39 +0700

    hapus

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$user="root";
-$pass="";
-$name="hiskia";
-$koneksi=mysqli_connect($host,$user,$pass) or die("koneksi ke database gagal");
-mysqli_select_db($koneksi,$name);
-?>
